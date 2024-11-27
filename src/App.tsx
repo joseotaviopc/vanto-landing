609f21c734180930d0bb5f77795c7c669b47ff1b
José Otavio Carvalho <carvalho.jose@gmail.com>
2024-11-27 20:11:42 -0300

    feat: use components to create a landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
+import { Header } from "./components/Header"
+import { Footer } from "./components/Footer"
+import { ParcelaItem } from "./components/ParcelaItem"
 
+function App() {
+  const parcelas = Array.from({ length: 20 });
   return (
     <>
-      <h1 className='text-3xl font-bold text-zinc-600 '>Landing Page</h1>
+      <main className=" text-white w-screen flex flex-col">
+        <Header />
+        
+        <div className="mt-36 p-6 h-[1000px] flex flex-col gap-2 z-0">
+          {parcelas.map((_, index) => <div key={index} className={index === parcelas.length -1 ? 'pb-36' : ''}><ParcelaItem /></div>)}
+        </div>
+
+        <Footer />
+      </main>
     </>
   )
 }
