ad9ba18a76887b34a4a373d5bb9fc25817eb86c1
José Otavio Carvalho <carvalho.jose@gmail.com>
2024-11-28 15:00:45 -0300

    fix: app to use parcelas list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
-import { ParcelaItem } from "./components/ParcelaItem"
+import { ParcelasList } from "./components/ParcelasList";
 
 function App() {
-  const parcelas = Array.from({ length: 20 });
+  
   return (
     <>
       <main className=" text-white w-screen flex flex-col">
         <Header />
         
-        <div className="mt-36 p-6 h-[1000px] flex flex-col gap-2 z-0">
-          {parcelas.map((_, index) => <div key={index} className={index === parcelas.length -1 ? 'pb-36' : ''}><ParcelaItem /></div>)}
-        </div>
+        <ParcelasList />
 
         <Footer />
       </main>
