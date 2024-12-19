a7696b216c4abc5c0329e4f76af19f0d8b25b3cc
José Otavio Carvalho <carvalho.jose@gmail.com>
2024-12-19 20:03:26 -0300

    revert: vite.config to upload in gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vanto-landing',
   plugins: [
     react(),
     tailwindcss(),
