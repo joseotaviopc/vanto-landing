6cedb66af14a9b5455fe0f80b177e4ccec8990ce
José Otavio Carvalho <carvalho.jose@gmail.com>
2024-12-19 19:01:35 -0300

    chore: add toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import './index.css'
 // import App from './App.tsx'
+import { ToastContainer } from 'react-toastify';
 import { Routes } from './routes/index.tsx';
 import { AuthProvider } from './context/index.tsx';
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <AuthProvider>
       <BrowserRouter>
         <Routes />
+        <ToastContainer />
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
