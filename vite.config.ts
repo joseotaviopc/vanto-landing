89fc0bca505f9d6760eddfbded05d581b3313b9a
José Otavio Carvalho <carvalho.jose@gmail.com>
2024-12-19 19:34:08 -0300

    fix: config base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vanto-landing',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
