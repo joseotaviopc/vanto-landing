367c055b4f61d2df90f9f654a67ab6b1ab4964bb
José Otavio Carvalho <carvalho.jose@gmail.com>
2024-12-19 19:35:14 -0300

    fix: remove app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import './index.css'
-// import App from './App.tsx'
 import { ToastContainer } from 'react-toastify';
 import { Routes } from './routes/index.tsx';
 import { AuthProvider } from './context/index.tsx';
