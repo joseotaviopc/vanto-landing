5cb7cd9e107cbf9b9edb2c548b876a6c8204da40
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-01-12 18:39:10 -0300

    fix: some fixes in parcela modal & modal pdf

--- src/components/ModalPdf.tsx
+++ src/components/ModalPdf.tsx
@@ -1,22 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClose } from '@fortawesome/free-solid-svg-icons'
-// Import the main component
 import { Viewer } from '@react-pdf-viewer/core';
 
-// Import the styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
-
 
 interface Props {
     handleClosePdf: () => void;
+    pdfUrl: string
 }
 
-export function ModalPdf({ handleClosePdf }: Props) {
-
+export function ModalPdf({ handleClosePdf, pdfUrl }: Props) {
     return (
         <div className="fixed left-0 top-0 bg-white h-screen w-screen z-[9999] flex flex-col overflow-hidden">
             <div className="flex items-center mb-4 p-6">
-                <button 
+                <button
                     onClick={handleClosePdf}
                     className="p-2 hover:bg-gray-100 rounded-full absolute left-4"
                 >
@@ -24,7 +21,7 @@ export function ModalPdf({ handleClosePdf }: Props) {
                 </button>
             </div>
             <div className='flex-auto overflow-auto'>
-                <Viewer  initialPage={1}  fileUrl="/pdf.pdf" />;
+                {pdfUrl ? <Viewer initialPage={1} fileUrl={pdfUrl} /> : null};
             </div>
         </div>
     );
--- src/components/ParcelaModal.tsx
+++ src/components/ParcelaModal.tsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { ParcelaOptions } from './ParcelaOptions';
 import { Titulo } from '../services/types';
 import { formatDate } from '../utils';
@@ -7,63 +5,47 @@ import { useAuth } from '../context/useAuth';
 
 interface Props {
     titulo: Titulo;
-    handleCloseModal: () => void;
+    handleCloseModal?: () => void;
     handleShowPdf: () => void;
 }
-export function ParcelaModal({ titulo, handleCloseModal, handleShowPdf }: Props) {
-    // console.log({titulo});
+export function ParcelaModal({ titulo, handleShowPdf }: Props) {
     const { user } = useAuth()
-    
+
     return (
-        <div className="fixed inset-0 bg-[rgb(0_0_0/0.1)] z-50 flex items-end justify-center">
-            <div className="bg-white w-full max-w-lg rounded-t-2xl p-2 transform transition-transform duration-300 ease-out">
-                <div className="flex items-center mb-4 p-6">
-                    <button 
-                        onClick={handleCloseModal}
-                        className="p-2 hover:bg-gray-100 rounded-full absolute left-4"
-                    >
-                        <FontAwesomeIcon icon={faArrowLeft} className="text-black text-xl" />
-                    </button>
-                    <h3 className="text-lg flex-auto text-center text-black font-semibold">Detalhes da Parcela</h3>
+        <div className="bg-white w-full rounded-t-2xl p-2 transform transition-transform duration-300 ease-out">
+            <div className="space-y-4 bg-[rgb(200_222_249)] rounded-sm p-4">
+                <div className='flex flex-col items-center gap-1 mb-8'>
+                    <span className={` px-2 py-1 rounded-md`}>{titulo.pagamento}</span>
+                    <span className="font-semibold text-black text-3xl">{titulo.valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</span>
                 </div>
 
-                <div className="space-y-4 bg-[rgb(200_222_249)] rounded-sm p-4">
-                    <div className='flex flex-col items-center gap-1 mb-8'>
-                        <span className={` px-2 py-1 rounded-md`}>{titulo.pagamento}</span>
-                        <span className="font-semibold text-black text-3xl">{titulo.valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</span>
-                    </div>
-
-                    <div className="flex flex-col items-center gap-1">
-                        <span className="text-gray-600 text-sm">Beneficiário</span>
-                        <span className="font-semibold text-black">Nacional G3 Consultoria e Assessoria Ltda</span>
-                    </div>
-                    <div className="flex flex-col items-center gap-1">
-                        <span className="text-gray-600 text-sm">Pagador</span>
-                        <span className="font-semibold uppercase text-black">{user?.name}</span>
-                    </div>
-                    
-                    <div className="flex justify-between text-center mt-8">
-                        <div className='flex flex-col gap-1'>
-                            <span className="text-gray-600 text-sm">Data de Vencimento</span>
-                            <span className='font-semibold text-black'>{formatDate(titulo.vencimento)}</span>
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <span className="text-gray-600 text-sm">Número da Parcela</span>
-                            <span className="font-semibold text-black">17/36</span>
-                        </div>
-                    </div>
-                    
+                <div className="flex flex-col items-center gap-1">
+                    <span className="text-gray-600 text-sm">Beneficiário</span>
+                    <span className="font-semibold text-black">Nacional G3 Consultoria e Assessoria Ltda</span>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <span className="text-gray-600 text-sm">Pagador</span>
+                    <span className="font-semibold uppercase text-black">{user?.name}</span>
                 </div>
 
-                <div className='flex flex-col items-center gap-2 p-8'>
-                    <span className='font-semibold text-black'>Código do boleto</span>
-                    <span className='text-gray-600 text-xl break-all text-center'>{titulo.codigo_boleto || 'Código do boleto não disponível'}</span>
+                <div className="flex justify-between text-center mt-8">
+                    <div className='flex flex-col gap-1'>
+                        <span className="text-gray-600 text-sm">Data de Vencimento</span>
+                        <span className='font-semibold text-black'>{formatDate(titulo.vencimento)}</span>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <span className="text-gray-600 text-sm">Número da Parcela</span>
+                        <span className="font-semibold text-black">{titulo.parcela}</span>
+                    </div>
                 </div>
 
-                {/* <pre className='text-red-800'>{JSON.stringify(titulo, null, 4)}</pre> */}
-
-                <ParcelaOptions handleShowPdf={handleShowPdf} />
             </div>
+
+            <div className='flex flex-col items-center gap-2 p-8'>
+                <span className='font-semibold text-black'>Código do boleto</span>
+                <span className='text-gray-600 text-xl break-all text-center'>{titulo.codigo_boleto || 'Código do boleto não disponível'}</span>
+            </div>
+            <ParcelaOptions handleShowPdf={handleShowPdf} titulo={titulo} />
         </div>
     );
 }
