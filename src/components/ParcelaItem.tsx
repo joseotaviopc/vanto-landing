10ae0ffc657c71364cbdf5c6aa4ca06e7c9549f2
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-01-12 18:38:26 -0300

    feat: add pagination in parcelas list & use shadcn to show parcela modal

--- src/components/ParcelaItem.tsx
+++ src/components/ParcelaItem.tsx
@@ -1,14 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileLines, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faFileLines, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { Titulo } from '../services/types';
 import { compareDate, formatDate } from '../utils';
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from './ui/button';
+import { ParcelaModal } from './ParcelaModal';
 
 interface Props {
     titulo: Titulo,
     handleOpenModal: () => void,
+    handleShowPdf: () => void
 }
 
-export function ParcelaItem({ titulo, handleOpenModal }: Props) {
+export function ParcelaItem({ titulo, handleOpenModal, handleShowPdf }: Props) {
     // const { status, color, text, date, value } = details;
     const status = compareDate(titulo.vencimento) ? 'Vencido' : 'Aberto';
     const statusText = titulo.pagamento ? 'Pago' : status;
@@ -28,20 +39,36 @@ export function ParcelaItem({ titulo, handleOpenModal }: Props) {
         },
     };
     return (
-        <>
+        <Dialog>
             <div className="flex w-full justify-between p-2 gap-4 bg-[rgb(46_81_130)] rounded-md">
                 <span className="flex items-center">
                     <FontAwesomeIcon icon={faFileLines} className='text-white text-2xl p-3 px-4 rounded-2xl bg-[rgb(92_120_156)] ' />
                 </span>
                 <div className="flex flex-col flex-auto justify-start gap-2">
                     <p className={`uppercase text-left font-medium rounded-xs w-fit ${colors[statusText].text} ${colors[statusText].color} p-0.5 text-[0.5rem]`}>{statusText}</p>
-                    <p className='font-bold'>{titulo.valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</p>
+                    <p className='font-bold'>{titulo.valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</p>
                     <p className='text-xs'>{formatDate(titulo.vencimento)}</p>
                 </div>
-                <button className="p-4 flex items-center" type='button' onClick={handleOpenModal}>
-                    <FontAwesomeIcon icon={faArrowRight} className='text-white text-2xl p-3 rounded-2xl border-2' />
-                </button>
+                <DialogTrigger asChild>
+                    {/* <Button variant="outline">Share</Button> */}
+                    <button className="p-4 flex items-center" type='button' onClick={handleOpenModal}>
+                        <FontAwesomeIcon icon={faArrowRight} className='text-white text-2xl p-3 rounded-2xl border-2' />
+                    </button>
+                </DialogTrigger>
             </div>
-        </>
+            <DialogContent className="sm:max-w-2xl flex flex-col items-center p-2">
+                <DialogHeader className='w-full px-4'>
+                    <div className='flex items-center justify-between w-full mb-4 p-6'>
+                        <DialogClose asChild className='absolute left-4'>
+                            <Button type="button" variant="secondary">
+                                <FontAwesomeIcon icon={faArrowLeft} className="text-black text-xl" />
+                            </Button>
+                        </DialogClose>
+                        <DialogTitle className='flex-1 text-center'>Detalhes da parcela</DialogTitle>
+                    </div>
+                </DialogHeader>
+                <ParcelaModal titulo={titulo} handleShowPdf={handleShowPdf} />
+            </DialogContent>
+        </Dialog>
     );
 }
--- src/components/ParcelasList.tsx
+++ src/components/ParcelasList.tsx
@@ -1,44 +1,115 @@
 import { useState } from "react";
 import { ParcelaItem } from "./ParcelaItem";
 import { ParcelasFilter } from "./ParcelasFilter";
-import { Titulo } from "../services/types";
-import { ParcelaModal } from "./ParcelaModal";
 import { compareDate } from "../utils";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
+import { GroupedTitulos } from "@/services/types";
+import { useAuth } from "@/context/useAuth";
 
-interface Props { 
+interface Pagination {
+    total: number;
+    page: number;
+    totalPages: number;
+}
+interface Props {
     handleShowPdf: () => void
-    titulos: Titulo[]
+    titulos: GroupedTitulos
     loading: boolean
+    pagination: Pagination
+    setPdfUrl: React.Dispatch<React.SetStateAction<string>>
+    currentContract: number | null
+}
+interface PaginationProps {
+    handleNextPage: (page: number) => void
+    handlePreviousPage: (page: number) => void
+    pagination: Pagination
 }
 
-export function ParcelasList({ handleShowPdf, titulos, loading }: Props) {
-    // const [isModalOpen, setIsModalOpen] = useState(false);
+function ParcelasPagination({ handleNextPage, handlePreviousPage, pagination }: PaginationProps) {
+    return (
+        <div className="w-full flex justify-center items-center gap-6 pb-4">
+            <p className={`text-zinc-900 w-full ${pagination.page > 1 ? 'text-right' : 'text-center'}`}>Página {pagination.page} de {pagination.totalPages}</p>
+            {pagination.page > 1 && (
+                <Pagination className="mx-0 justify-start">
+                    <PaginationContent>
+                        {pagination.page > 1 && (
+                            <PaginationItem className="text-zinc-900">
+                                <PaginationPrevious size="sm" href="#" onClick={() => handlePreviousPage(pagination.page - 1)} />
+                            </PaginationItem>
+                        )}
+                        <PaginationItem className="text-zinc-900">
+                            <PaginationLink href="#" isActive={pagination.page !== 1} onClick={() => handlePreviousPage(1)}>1</PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem className="text-zinc-900">
+                            <PaginationEllipsis />
+                        </PaginationItem>
+                        <PaginationItem className="text-zinc-900">
+                            <PaginationLink href="#" isActive={pagination.totalPages !== pagination.page} onClick={() => handleNextPage(pagination.totalPages)}>{pagination.totalPages}</PaginationLink>
+                        </PaginationItem>
+                        {pagination.page < pagination.totalPages && (
+                            <PaginationItem className="text-zinc-900">
+                                <PaginationNext href="#" onClick={() => handleNextPage(pagination.page + 1)} />
+                            </PaginationItem>
+                        )}
+                    </PaginationContent>
+                </Pagination>
+            )}
+        </div>
+    )
+}
+
+export function ParcelasList({
+    handleShowPdf,
+    titulos,
+    loading,
+    pagination,
+    setPdfUrl,
+    currentContract
+}: Props) {
     const [statusParcela, setStatusParcela] = useState({
         all: true,
         abertas: false,
         atrasadas: false,
         pagas: false
     });
-    const [selectedTituloId, setSelectedTituloId] = useState<string | null>(null);
+    const [, setSelectedTituloId] = useState<string | null>(null);
+    const { getTitulos } = useAuth()
 
+    const titulosByContract = currentContract ? titulos[currentContract] : [];
     const statusCounts = {
-        all: titulos.length,
-        abertas: titulos.filter(titulo => !titulo.pagamento && !compareDate(titulo.vencimento)).length,
-        atrasadas: titulos.filter(titulo => !titulo.pagamento && compareDate(titulo.vencimento)).length,
-        pagas: titulos.filter(titulo => titulo.pagamento).length
+        all: titulosByContract.length,
+        abertas: titulosByContract.filter(titulo => !titulo.pagamento && !compareDate(titulo.vencimento)).length,
+        atrasadas: titulosByContract.filter(titulo => !titulo.pagamento && compareDate(titulo.vencimento)).length,
+        pagas: titulosByContract.filter(titulo => titulo.pagamento).length
     };
 
     function handleOpenModal(tituloId: string) {
         setSelectedTituloId(tituloId);
     }
 
-    function handleCloseModal() {
-        setSelectedTituloId(null);
+    function handleNextPage(page: number) {
+        if (pagination.page === pagination.totalPages) return
+        getTitulos({ page, limit: 30 })
     }
 
-    const filteredTitulos = titulos.filter(titulo => {
+    function handlePreviousPage(page: number) {
+        if (pagination.page === 1) return
+        if (pagination.page > 1) {
+            getTitulos({ page, limit: 30 })
+        }
+    }
+
+    const filteredTitulos = titulosByContract.filter(titulo => {
         if (statusParcela.all) return true;
         if (statusParcela.pagas && titulo.pagamento) return true;
         if (statusParcela.atrasadas && !titulo.pagamento && compareDate(titulo.vencimento)) return true;
@@ -58,15 +129,15 @@ export function ParcelasList({ handleShowPdf, titulos, loading }: Props) {
         <>
             <ParcelasFilter statusParcela={statusParcela} setStatusParcela={setStatusParcela} statusCounts={statusCounts} />
             <div className="p-6 h-[1000px] flex flex-col gap-2 z-0">
-            {filteredTitulos?.map((titulo, index) => 
-                <div key={index} className={index === filteredTitulos.length -1 ? 'pb-36' : ''}>
-                    <ParcelaItem titulo={titulo} handleOpenModal={() => handleOpenModal(String(titulo.id_titulo))} />
-                    {/* Bottom Modal */}
-                    {selectedTituloId === String(titulo.id_titulo) && (
-                        <ParcelaModal titulo={titulo} handleCloseModal={handleCloseModal} handleShowPdf={handleShowPdf} />
-                    )}
-                </div>
-            )}
+                {filteredTitulos?.map((titulo, index) =>
+                    <div key={index} className={index === filteredTitulos.length - 1 ? 'pb-12' : ''}>
+                        <ParcelaItem titulo={titulo} handleOpenModal={() => handleOpenModal(String(titulo.id_titulo))} handleShowPdf={() => {
+                            handleShowPdf()
+                            setPdfUrl(titulo.link_boleto)
+                        }} />
+                    </div>
+                )}
+                <ParcelasPagination pagination={pagination} handleNextPage={handleNextPage} handlePreviousPage={handlePreviousPage} />
             </div>
         </>
     )
